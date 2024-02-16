68c31cb45ce6a6f34e87c8158c1b44e909441f03
John Gordon <jcg4sg@virginia.edu>
2024-02-15 20:10:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.title('2023 Full Year Pull')
 data['Two'] = 2
 
 
-st.dataframe(data.iloc[:, :])
+st.dataframe(data.iloc[:, :-3])
 
 # f = (
 #     Pie(init_opts=opts.InitOpts(theme=ThemeType.DARK))
