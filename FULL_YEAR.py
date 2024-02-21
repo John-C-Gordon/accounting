475c964284cc7d152fb6f23d7e9c14f09540a0ec
John Gordon <jcg4sg@virginia.edu>
2024-02-21 04:18:05 -0500

    Update FULL_YEAR.py

--- FULL_YEAR.py
+++ FULL_YEAR.py
@@ -12,9 +12,6 @@ import pymysql
 st.set_page_config(page_icon="📊", page_title="Accounting Data Query")
 api_token = st.secrets["api_token"]
 
-#connect 
-conn = st.connection('mysql', type='sql')
-
 class StreamlitResponse(ResponseParser):
     def __init__(self, context) -> None:
         super().__init__(context)
@@ -31,20 +28,25 @@ class StreamlitResponse(ResponseParser):
         st.write(result["value"])
         return
 
-st.title('2023 Full Year Pull')
-
-gf = pd.DataFrame(conn.query('select * from data_pull;', ttl=0))
-df = pd.DataFrame(conn.query('select * from codes;', ttl=0))
-gf['appt_time'] = df['screening_cd']
-gf['payment_uid'] = df['appointment_cd']
-
-end_date = datetime(2023, 12, 31, 0)
-gf['Comment_Alert'] = pd.to_datetime(gf['Comment_Alert'])
-gf['earned'] = gf['Comment_Alert'] < end_date
-
-gf.rename(columns={'amount_paid': 'Amount Paid', 'Comment_Alert': 'Appointment Date', 'screening_id': 'Payment Type', 
-                   'payment_type_id': 'Payment UID', 'appt_time': 'Screening Code', 'payment_uid': 'Appoitment Code', 'screening_cd': 'Screening ID'}, inplace=True)
-st.dataframe(gf)
+#connect 
+@st.cache_data()
+def fetch_data():
+    conn = st.connection('mysql', type='sql')
+    
+    st.title('2023 Full Year Pull')
+    
+    gf = pd.DataFrame(conn.query('select * from data_pull;', ttl=0))
+    df = pd.DataFrame(conn.query('select * from codes;', ttl=0))
+    gf['appt_time'] = df['screening_cd']
+    gf['payment_uid'] = df['appointment_cd']
+    
+    end_date = datetime(2023, 12, 31, 0)
+    gf['Comment_Alert'] = pd.to_datetime(gf['Comment_Alert'])
+    gf['earned'] = gf['Comment_Alert'] < end_date
+    
+    gf.rename(columns={'amount_paid': 'Amount Paid', 'Comment_Alert': 'Appointment Date', 'screening_id': 'Payment Type', 
+                       'payment_type_id': 'Payment UID', 'appt_time': 'Screening Code', 'payment_uid': 'Appoitment Code', 'screening_cd': 'Screening ID'}, inplace=True)
+    return gf
 
 query = st.text_area('What query would you like to run?')
 
