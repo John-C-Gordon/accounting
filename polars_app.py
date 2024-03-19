be78c5677c057c5de6be68bc7f1085bc1b8f4e72
John Gordon <jcg4sg@virginia.edu>
2024-03-19 19:58:30 -0400

    Update polars_app.py

--- polars_app.py
+++ polars_app.py
@@ -23,7 +23,7 @@ conn = create_connection()
 cursor = conn.cursor()
 @st.cache_data
 def get_gf():
-    gf = pl.read_database("SELECT * FROM data_pull;", conn)
+    gf = pl.read_database("SELECT * FROM appt_codes;", conn)
     return gf
 gf = get_gf()
 # st.write(conn)
