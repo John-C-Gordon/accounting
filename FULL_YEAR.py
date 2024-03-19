b969806dd3007528cb1186523c52f8526a2a82b6
John Gordon <jcg4sg@virginia.edu>
2024-03-19 15:56:03 -0400

    Update FULL_YEAR.py

--- FULL_YEAR.py
+++ FULL_YEAR.py
@@ -64,6 +64,7 @@ if authentication_status == True:
     # @st.cache_data
     # def get_gf():
     # gf = pl.read_database("SELECT * FROM data_pull WHERE amount_paid < 0;", conn)
+    pd.read_sql_table("data_pull", conn)
     
     ctx = pl.SQLContext(register_globals=True, eager_execution=True)
     # gf = get_gf()
