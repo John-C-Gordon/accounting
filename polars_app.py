7359d71d0bff59be6206320d5e140d979f7a83ae
John Gordon <jcg4sg@virginia.edu>
2024-03-20 13:45:46 -0400

    Update polars_app.py

--- polars_app.py
+++ polars_app.py
@@ -23,11 +23,11 @@ conn = create_connection()
 cursor = conn.cursor()
 # @st.cache_resource
 # def get_gf():
-gf = pl.read_database("SELECT * FROM data_pull;", conn)
-ctx = pl.SQLContext(data_pull = gf)
+# gf = pl.read_database("SELECT * FROM data_pull;", conn)
+# ctx = pl.SQLContext(data_pull = gf)
     # return ctx, gf
 # ctx, gf = get_gf()
-st.dataframe(ctx.execute(
-    '''
-    SELECT * FROM data_pull LIMIT 10;
-    ''', eager = True))
+# st.dataframe(ctx.execute(
+#     '''
+#     SELECT * FROM data_pull LIMIT 10;
+#     ''', eager = True))
