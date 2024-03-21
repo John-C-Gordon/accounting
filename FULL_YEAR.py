49cd3573f267107131d4c95add21b8d7d494da8b
John Gordon <jcg4sg@virginia.edu>
2024-03-21 13:52:06 -0400

    Update FULL_YEAR.py

--- FULL_YEAR.py
+++ FULL_YEAR.py
@@ -58,11 +58,11 @@ if authentication_status == True:
     @st.cache_data
     def get_gf():
         gf = pl.DataFrame(conn.query('select * from data_pull LIMIT 10;', ttl=0))
-        ctx = pl.SQLContext(data=gf)
+        
         return gf, ctx
-    gf, ctx = get_gf()
-    # gf = get_gf()
+    gf = get_gf()
     
+    ctx = pl.SQLContext(data=gf)
     st.write(gf)
     
     # st.header('Find row(s) by:')
