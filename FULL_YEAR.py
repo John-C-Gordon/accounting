c152ddf2a5274f7950d40ca163b3b64d124c94e8
John Gordon <jcg4sg@virginia.edu>
2024-03-24 19:09:08 -0400

    Update FULL_YEAR.py

--- FULL_YEAR.py
+++ FULL_YEAR.py
@@ -61,23 +61,23 @@ if authentication_status == True:
         participant_guid = conn.query('select participant_guid from additional_fields;', ttl=0)
         order_guid = conn.query('select order_guid from additional_fields;', ttl=0)
         appt_codes = conn.query('select * from appt_codes;', ttl=0)
-        gf.insert_column(5, pl.Series(participant_guid["participant_guid"]))
-        gf.insert_column(6, pl.Series(order_guid["order_guid"]))
-        gf.insert_column(7, pl.Series(appt_codes["Appointment_Code"]))
-        end_date = datetime(2023, 12, 31, 0)
-        gf = gf.with_columns(pl.when(pl.col("Appointment Date") > end_date).then(True).otherwise(False).alias("Earned"))
+        # gf.insert_column(5, pl.Series(participant_guid["participant_guid"]))
+        # gf.insert_column(6, pl.Series(order_guid["order_guid"]))
+        # gf.insert_column(7, pl.Series(appt_codes["Appointment_Code"]))
+        # end_date = datetime(2023, 12, 31, 0)
+        # gf = gf.with_columns(pl.when(pl.col("Appointment Date") > end_date).then(True).otherwise(False).alias("Earned"))
         return gf
     gf = get_gf()
 
     # participant_guid = conn.query('select participant_guid from additional_fields;', ttl=0)
     # order_guid = conn.query('select order_guid from additional_fields;', ttl=0)
     # appt_codes = conn.query('select * from appt_codes;', ttl=0)
-    # gf.insert_column(5, pl.Series(participant_guid["participant_guid"]))
-    # gf.insert_column(6, pl.Series(order_guid["order_guid"]))
-    # gf.insert_column(7, pl.Series(appt_codes["Appointment_Code"]))
+    gf.insert_column(5, pl.Series(participant_guid["participant_guid"]))
+    gf.insert_column(6, pl.Series(order_guid["order_guid"]))
+    gf.insert_column(7, pl.Series(appt_codes["Appointment_Code"]))
     
-    # end_date = datetime(2023, 12, 31, 0)
-    # gf = gf.with_columns(pl.when(pl.col("Appointment Date") > end_date).then(True).otherwise(False).alias("Earned")
+    end_date = datetime(2023, 12, 31, 0)
+    gf = gf.with_columns(pl.when(pl.col("Appointment Date") > end_date).then(True).otherwise(False).alias("Earned")
     
     st.dataframe(gf.head())
     
