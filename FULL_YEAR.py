195bc8b9252e3dd83ff15ced9152f14efc3d7fe1
John Gordon <jcg4sg@virginia.edu>
2024-03-25 08:26:44 -0400

    Update FULL_YEAR.py

--- FULL_YEAR.py
+++ FULL_YEAR.py
@@ -64,13 +64,10 @@ if authentication_status == True:
         # gf.insert_column(5, pl.Series(participant_guid["participant_guid"]))
         # gf.insert_column(6, pl.Series(order_guid["order_guid"]))
         # gf.insert_column(7, pl.Series(appt_codes["Appointment_Code"]))
-        # end_date = datetime(2023, 12, 31, 0)
-        # gf = gf.with_columns(pl.when(pl.col("Appointment Date") > end_date).then(True).otherwise(False).alias("Earned"))
+        end_date = datetime(2023, 12, 31, 0)
+        gf = gf.with_columns(pl.when(pl.col("Appointment Date") < end_date).then(True).otherwise(False).alias("Earned"))
         return gf
     gf = get_gf()
-    
-    end_date = datetime(2023, 12, 31, 0)
-    gf = gf.with_columns(pl.when(pl.col("Appointment Date") < end_date).then(True).otherwise(False).alias("Earned"))
     
     st.dataframe(gf.head())
     
