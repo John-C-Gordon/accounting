5e85186886b64a08a008b453f4a6221fc1bf4741
John Gordon <jcg4sg@virginia.edu>
2024-03-25 08:40:39 -0400

    Update FULL_YEAR.py

--- FULL_YEAR.py
+++ FULL_YEAR.py
@@ -109,6 +109,6 @@ if authentication_status == True:
     if len(fields) != 0:
         if submitted:
             st.write(s[:-3])
-            # st.write(ctx.execute('''SELECT * FROM data WHERE {}'''.format(s[:-1]))
+            st.dataframe(ctx.execute('''SELECT * FROM data WHERE {}'''.format(s[:-3]))
             # st.success("{} rows returned.".format(len(gf.query("{}".format(s[:-1])).index)))
 
