3098c7df8c59af6254cd8853dca987cea15532d0
John Gordon <jcg4sg@virginia.edu>
2024-03-25 09:33:55 -0400

    Update FULL_YEAR.py

--- FULL_YEAR.py
+++ FULL_YEAR.py
@@ -77,7 +77,9 @@ if authentication_status == True:
     
     col1, col2 = st.columns(2)
     with col1:
-        for i in (gf.columns[2:4] + gf.columns[0]):
+        for i in gf.columns[2:4]:
+            option = st.text_input('{}:'.format(i), key='{}'.format(i))
+        for i in gf.columns[0]:
             option = st.text_input('{}:'.format(i), key='{}'.format(i))
     with col2:
         for i in gf.columns[4:8]:
