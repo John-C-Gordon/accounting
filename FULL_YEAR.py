33ccbe8bb00e602a1ea759f1ba1bac1c0232d45c
John Gordon <jcg4sg@virginia.edu>
2024-03-27 19:03:42 -0400

    Update FULL_YEAR.py

--- FULL_YEAR.py
+++ FULL_YEAR.py
@@ -77,7 +77,7 @@ if authentication_status == True:
     gf = get_gf()
 
     
-    selected = option_menu(options=['Analysis', 'Search', 'Smart Query'], icons=['clipboard2-data', 'search', 'magic'],
+    selected = option_menu(menu_title=None, options=['Analysis', 'Search', 'Smart Query'], icons=['clipboard2-data', 'search', 'magic'],
                            default_index=1, orientation='horizontal', key='Menu') 
     # st.write(selected)
     if selected == 'Search':
