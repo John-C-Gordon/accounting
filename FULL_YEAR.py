b423d9cbe65721476efe0a6e86f8410736e2e944
John Gordon <jcg4sg@virginia.edu>
2024-03-26 20:40:46 -0400

    Update FULL_YEAR.py

--- FULL_YEAR.py
+++ FULL_YEAR.py
@@ -131,8 +131,8 @@ if authentication_status == True:
                 .add_yaxis("商家A", [10, 20, 30, 40, 50, 40])
                 .add_yaxis("商家B", [20, 10, 40, 30, 40, 50])
                 .set_global_opts(
-                    xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=-15)),
-                    title_opts=opts.TitleOpts(title="Bar-旋转X轴标签", subtitle="解决标签名字过长的问题"),
+                    xaxis_opts=pyecharts.options.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=-15)),
+                    title_opts=pyecharts.options.TitleOpts(title="Bar-旋转X轴标签", subtitle="解决标签名字过长的问题"),
                 )
                 .render("bar_rotate_xaxis_label.html")
             )
