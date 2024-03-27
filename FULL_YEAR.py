4b7deafe1681ac32dd53cd520cd68b6873046c2a
John Gordon <jcg4sg@virginia.edu>
2024-03-26 20:44:17 -0400

    Update FULL_YEAR.py

--- FULL_YEAR.py
+++ FULL_YEAR.py
@@ -12,6 +12,7 @@ import streamlit_authenticator as stauth
 import polars as pl
 from pyecharts.charts import Bar
 from pyecharts.globals import ThemeType
+from pyecharts import options as opts
 
 #TITLE
 
@@ -131,8 +132,8 @@ if authentication_status == True:
                 .add_yaxis("商家A", [10, 20, 30, 40, 50, 40])
                 .add_yaxis("商家B", [20, 10, 40, 30, 40, 50])
                 .set_global_opts(
-                    xaxis_opts=pyecharts.options.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=-15)),
-                    title_opts=pyecharts.options.TitleOpts(title="Bar-旋转X轴标签", subtitle="解决标签名字过长的问题"),
+                    xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=-15)),
+                    title_opts=opts.TitleOpts(title="Bar-旋转X轴标签", subtitle="解决标签名字过长的问题"),
                 )
                 .render("bar_rotate_xaxis_label.html")
             )
