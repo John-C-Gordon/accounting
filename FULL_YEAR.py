9af9590e311926f79e8e73adf7d941d311824d8f
John Gordon <jcg4sg@virginia.edu>
2024-03-27 11:46:37 -0400

    Update FULL_YEAR.py

--- FULL_YEAR.py
+++ FULL_YEAR.py
@@ -148,7 +148,7 @@ if authentication_status == True:
             .add(
                 "Earned vs. Unearned Revenue",
                 [list(z) for z in zip(earned_unearned["Earned"].to_list(), earned_unearned["Total Revenue"].to_list())],
-                # radius=["25%", "50%"],
+                radius=["25%", "50%"],
                 center=["25%", "35%"],
                 label_opts=opts.LabelOpts(is_show=True),
             )
